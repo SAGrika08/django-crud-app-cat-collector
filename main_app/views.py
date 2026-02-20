b30b3417e0c8bed11d2921feb3ca65a33d5ec8e8
Sagrika Trehan <trehansagrika0897@gmail.com>
2026-02-20 20:23:28 +0530

    commit

--- main_app/views.py
+++ main_app/views.py
@@ -1,22 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# views.py
-
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
+from .models import Cat
 
 
 # Create your views here.
@@ -28,6 +13,10 @@ def about(request):
 
 
 def cat_index(request):
-    # Render the cats/index.html template with the cats data
+    cats = Cat.objects.all() 
     return render(request, 'cats/index.html', {'cats': cats})
+
+def cat_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', {'cat': cat})
 
