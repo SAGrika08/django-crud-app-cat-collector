994f1ddd15a5fa902e66e2f6d3ffff6987b88466
Sagrika Trehan <trehansagrika0897@gmail.com>
2026-02-25 23:22:59 +0530

    com

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 
 from .models import Cat, Toy
@@ -10,8 +16,11 @@ from .forms import FeedingForm
 
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html')
+# def home(request):
+#     return render(request, 'home.html')
+class Home(LoginView):
+    template_name = 'home.html'
+
 
 def about(request):
     return render(request, 'about.html')
@@ -21,14 +30,18 @@ def about(request):
 #     cats = Cat.objects.all() 
 #     return render(request, 'cats/index.html', {'cats': cats})
 
-class CatList(ListView):
+
+class CatList( LoginRequiredMixin, ListView):
     model = Cat
+
+    def get_queryset(self):
+        return Cat.objects.filter(user=self.request.user)
 
 # def cat_detail(request, cat_id):
 #     cat = Cat.objects.get(id=cat_id)
 #     return render(request, 'cats/detail.html', {'cat': cat})
 
-class CatDetail(DetailView):
+class CatDetail( LoginRequiredMixin, DetailView):
     model = Cat
 
     def get_context_data(self, **kwargs):
@@ -38,20 +51,29 @@ class CatDetail(DetailView):
      context['toys'] = toys_cat_doesnt_have
      return context
 
-class CatCreate(CreateView):
+class CatCreate(LoginRequiredMixin, CreateView):
     model = Cat
     fields = ['name', 'breed', 'description', 'age']
     # success_url = reverse_lazy('cat-index')
 
-class CatUpdate(UpdateView):
+    # This inherited method is called when a
+    # valid cat form is being submitted
+    def form_valid(self, form):
+        # Assign the logged in user (self.request.user)
+        form.instance.user = self.request.user  # form.instance is the cat
+        # Let the CreateView do its job as usual
+        return super().form_valid(form)
+
+class CatUpdate(LoginRequiredMixin, UpdateView):
     model = Cat
     # Let's disallow the renaming of a cat by excluding the name field!
     fields = ['breed', 'description', 'age']
 
-class CatDelete(DeleteView):
+class CatDelete(LoginRequiredMixin, DeleteView):
     model = Cat
     success_url = reverse_lazy('cat-index')
 
+@login_required
 def add_feeding(request, pk):
     form = FeedingForm(request.POST)
     if form.is_valid():
@@ -60,29 +82,31 @@ def add_feeding(request, pk):
         new_feeding.save()
     return redirect('cat-detail', pk=pk)
 
-class ToyCreate(CreateView):
+class ToyCreate( LoginRequiredMixin, CreateView):
     model = Toy
     fields = '__all__'
 
-class ToyList(ListView):
+class ToyList( LoginRequiredMixin, ListView):
     model = Toy
 
-class ToyDetail(DetailView):
+class ToyDetail( LoginRequiredMixin, DetailView):
     model = Toy
 
-class ToyUpdate(UpdateView):
+class ToyUpdate(LoginRequiredMixin, UpdateView):
     model = Toy
     fields = ['name', 'color']
 
-class ToyDelete(DeleteView):
+class ToyDelete(LoginRequiredMixin, DeleteView):
     model = Toy
     success_url = reverse_lazy('toy-index')
 
+@login_required
 def associate_toy(request, cat_id, toy_id):
     # Note that you can pass a toy's id instead of the whole object
     Cat.objects.get(id=cat_id).toys.add(toy_id)
     return redirect('cat-detail',pk=cat_id)
 
+@login_required
 def remove_toy(request, cat_id, toy_id):
     # Look up the cat
     # Look up the toy
@@ -90,3 +114,27 @@ def remove_toy(request, cat_id, toy_id):
     Cat.objects.get(id=cat_id).toys.remove(toy_id)
     return redirect('cat-detail', pk=cat_id)
 
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        # This is how to create a 'user' form object
+        # that includes the data from the browser
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            # This will add the user to the database
+            user = form.save()
+            # This is how we log a user in
+            login(request, user)
+            return redirect('cat-index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    # A bad POST or a GET request, so render signup.html with an empty form
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'signup.html', context)
+    # Same as: 
+    # return render(
+    #     request, 
+    #     'signup.html',
+    #     {'form': form, 'error_message': error_message}
+    # )
